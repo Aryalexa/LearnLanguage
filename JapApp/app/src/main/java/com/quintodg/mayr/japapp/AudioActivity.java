df991730ed62745ace2b1d8f1ffa523cb8178e26
Aryalexa <aryalexa@gmail.com>
2016-03-27 20:14:31 +0200

    Record with one button
    Audio activity changes. It uses the layout now.

--- JapApp/app/src/main/java/com/quintodg/mayr/japapp/AudioActivity.java
+++ JapApp/app/src/main/java/com/quintodg/mayr/japapp/AudioActivity.java
@@ -1,180 +1,127 @@
 package com.quintodg.mayr.japapp;
 
-/*
- * SRC: http://developer.android.com/intl/es/guide/topics/media/audio-capture.html
- * The application needs to have the permission to write to external storage
- * if the output file is written to the external storage, and also the
- * permission to record audio. These permissions must be set in the
- * application's AndroidManifest.xml file, with something like:
- *
- * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
- * <uses-permission android:name="android.permission.RECORD_AUDIO" />
- *
- */
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.view.View;
-import android.view.ViewGroup;
-import android.util.Log;
+import android.widget.Button;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+import android.widget.TextView;
 
-import java.io.IOException;
+import java.io.File;
 
 
 public class AudioActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
 
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton mPlayButton = null;
-    private MediaPlayer mPlayer = null;
-
-    public AudioActivity() {
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-    }
+    private Button btnRecord;
+    private TextView textRecord;
+    private MediaRecorder recorder;
+    private String FILE;
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // no se usa el layout activity_audio
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
-        //setContentView(R.layout.activity_audio);
+        setContentView(R.layout.activity_audio);
 
         // toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-    }
+        FILE = Environment.getExternalStorageDirectory() + "/audioTest.3gpp";
+        textRecord = (TextView)findViewById(R.id.textRecord);
 
-
-    /** RECORD **/
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
+        btnRecord = (Button)findViewById(R.id.btnRecord);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                onRecord(mStartRecording); // main process
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
+                if (btnRecord.getText().toString().equals("Record"))
+                {
+                    try {
+                        startRecord();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    textRecord.setText("Recording...");
+
+                    btnRecord.setText("End");
+                } else if (btnRecord.getText().toString().equals("End"))
+                {
+                    endRecord();
+                    textRecord.setText("");
+                    btnRecord.setText("Play");
+                } else if (btnRecord.getText().toString().equals("Play"))
+                {
+                    try {
+                        startPlayback();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    btnRecord.setText("Stop");
+                } else if (btnRecord.getText().toString().equals("Stop"))
+                {
+                    stopPlayback();
+                    btnRecord.setText("Record");
                 }
-                mStartRecording = !mStartRecording;
             }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
+        });
     }
 
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
+    public void startRecord() throws Exception
+    {
+        if (recorder !=null)
+        {
+            recorder.release();
         }
 
-        mRecorder.start();
+        File fileOut = new File(FILE);
+        if (fileOut!=null)
+        {
+            fileOut.delete(); // overwrite any existing file
+        }
+
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(FILE);
+
+        recorder.prepare();
+        recorder.start();
     }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
+    public void endRecord()
+    {
+        recorder.stop();
+        recorder.release();
     }
-
-
-    /** PLAY **/
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying); // main process
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
+    public void startPlayback() throws Exception
+    {
+        if (player!=null)
+        {
+            player.stop();
+            player.release();
+        }
+        player = new MediaPlayer();
+        player.setDataSource(FILE);
+        player.prepare();
+        player.start();
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() { // when sound ends
+            @Override
+            public void onCompletion(MediaPlayer player) {
+                player.release(); // release resources
             }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
+        });
     }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
+    public void stopPlayback()
+    {
+        player.stop();
+        player.release();
     }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
 
 }
